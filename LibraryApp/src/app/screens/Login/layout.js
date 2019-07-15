eaf65a7cf1c2bac093128e46b61adb06e4896a79
Emmanuel Estigarribia <eestigarribia@Woloxs-Mac-mini-Emma.local>
2019-07-15 11:37:38 -0300

    Added KeyboardAvoidingView to login screen

--- LibraryApp/src/app/screens/Login/layout.js
+++ LibraryApp/src/app/screens/Login/layout.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { ImageBackground, Image, Text } from 'react-native';
+import { ImageBackground, Image, Text, KeyboardAvoidingView } from 'react-native';
 import PropTypes from 'prop-types';
 import CustomButton from '@components/CustomButton';
 import CustomTextInput from '@components/CustomTextInput';
@@ -20,36 +20,38 @@ class Login extends Component {
   render() {
     const { onSubmit, onPasswordChange, onEmailChange, emailError, passwordError, authError } = this.props;
     return (
-      <ImageBackground source={backgroundImg} style={styles.container}>
-        <Image source={logo} style={styles.logo} />
-        <CustomTextInput
-          title={EMAIL}
-          placeholder={EMAIL_PLACEHOLDER}
-          viewStyles={styles.inputStyle}
-          titleStyles={styles.titleStyle}
-          inputTextStyles={styles.inputTextStyle}
-          onChange={onEmailChange}
-        />
-        {emailError && <Text style={styles.errorText}>{emailError}</Text>}
-        <CustomTextInput
-          title={PASSWORD}
-          placeholder={PASSWORD_PLACEHOLDER}
-          viewStyles={styles.inputStyle}
-          titleStyles={styles.titleStyle}
-          inputTextStyles={styles.inputTextStyle}
-          onChange={onPasswordChange}
-          secureTextEntry
-        />
-        {passwordError && <Text style={styles.errorText}>{passwordError}</Text>}
-        <CustomButton
-          title={SUBMIT_TEXT}
-          buttonStyles={styles.buttonStyle}
-          textStyles={styles.buttonTextStyle}
-          onPress={onSubmit}
-        />
-        {!!authError && <Text style={styles.errorTextGeneral}>{authError}</Text>}
-        <Text style={styles.footerText}>{FOOTER_TEXT}</Text>
-      </ImageBackground>
+      <KeyboardAvoidingView behavior="height" style={styles.keyboardAvoidingContainer}>
+        <ImageBackground source={backgroundImg} style={styles.container}>
+          <Image source={logo} style={styles.logo} />
+          <CustomTextInput
+            title={EMAIL}
+            placeholder={EMAIL_PLACEHOLDER}
+            viewStyles={styles.inputStyle}
+            titleStyles={styles.titleStyle}
+            inputTextStyles={styles.inputTextStyle}
+            onChange={onEmailChange}
+          />
+          {emailError && <Text style={styles.errorText}>{emailError}</Text>}
+          <CustomTextInput
+            title={PASSWORD}
+            placeholder={PASSWORD_PLACEHOLDER}
+            viewStyles={styles.inputStyle}
+            titleStyles={styles.titleStyle}
+            inputTextStyles={styles.inputTextStyle}
+            onChange={onPasswordChange}
+            secureTextEntry
+          />
+          {passwordError && <Text style={styles.errorText}>{passwordError}</Text>}
+          <CustomButton
+            title={SUBMIT_TEXT}
+            buttonStyles={styles.buttonStyle}
+            textStyles={styles.buttonTextStyle}
+            onPress={onSubmit}
+          />
+          {!!authError && <Text style={styles.errorTextGeneral}>{authError}</Text>}
+          <Text style={styles.footerText}>{FOOTER_TEXT}</Text>
+        </ImageBackground>
+      </KeyboardAvoidingView>
     );
   }
 }
