c51d4ca4e608bd4f3cdc91cb694d9eb3e004ecae
Emmanuel Estigarribia <eestigarribia@Woloxs-Mac-mini-Emma.local>
2019-07-29 09:19:06 -0300

    Test 3. Pow returning undefined if there is just one argument

--- finger-exercises-react/testing_1/01 - matchers/test.js
+++ finger-exercises-react/testing_1/01 - matchers/test.js
@@ -6,9 +6,9 @@ describe('01 - matchers', () => {
   });
   it('pow returns undefined if there is no arguments', () => {
     expect(pow()).toBeUndefined();
-    expect(pow(3)).toBeUndefined();
   });
-  xit('pow returns undefined if there is just one argument', () => {
+  it('pow returns undefined if there is just one argument', () => {
+    expect(pow(3)).toBeUndefined();
   });
   xit('pow returns an array of power results if array of pairs are sent as arguments', () => {
   });
