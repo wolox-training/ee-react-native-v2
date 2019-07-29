6be846a707b177edf2230a03efd9864a095ead2b
Emmanuel Estigarribia <eestigarribia@Woloxs-Mac-mini-Emma.local>
2019-07-29 09:50:46 -0300

    Test 5. Undefined is thrown if an argument is not a pair as expected

--- finger-exercises-react/testing_1/01 - matchers/test.js
+++ finger-exercises-react/testing_1/01 - matchers/test.js
@@ -13,6 +13,7 @@ describe('01 - matchers', () => {
   it('pow returns an array of power results if array of pairs are sent as arguments', () => {
     expect(pow([2, 3], [2, 4], [2, 5])).toEqual([8, 16, 32]);
   });
-  xit('pow returns undefined in the right position of the result array if pair is not as expected', () => {
+  it('pow returns undefined in the right position of the result array if pair is not as expected', () => {
+    expect(pow([2, 3], [2, 4, 5], [2, 4])).toEqual([8, undefined, 16]);
   });
 });
