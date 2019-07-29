7ff857174752cba2ff645c3dd8e65837adb3780f
Emmanuel Estigarribia <eestigarribia@Woloxs-Mac-mini-Emma.local>
2019-07-29 09:14:22 -0300

    First test. Pow doing the power

---
+++ finger-exercises-react/testing_1/01 - matchers/test.js
@@ -0,0 +1,15 @@
+import pow from '.';
+
+describe('01 - matchers', () => {
+  it('pow returns the power based on two numeric arguments', () => {
+    expect(pow(2, 3)).toBe(8);
+  });
+  xit('pow returns undefined if there is no arguments', () => {
+  });
+  xit('pow returns undefined if there is just one argument', () => {
+  });
+  xit('pow returns an array of power results if array of pairs are sent as arguments', () => {
+  });
+  xit('pow returns undefined in the right position of the result array if pair is not as expected', () => {
+  });
+});
