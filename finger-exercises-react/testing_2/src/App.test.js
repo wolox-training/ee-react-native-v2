e5a48667588c48c9df3f81076c200160ddbde5e5
Emmanuel Estigarribia <eestigarribia@Woloxs-Mac-mini-Emma.local>
2019-07-30 10:49:04 -0300

    App.test.js renders without crashing

--- finger-exercises-react/testing_2/src/App.test.js
+++ finger-exercises-react/testing_2/src/App.test.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { configure, mount } from 'enzyme';
+import { configure, mount, shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import Adapter from 'enzyme-adapter-react-16';
 
 import App from './App';
+import Dashboard from './screens/Dashboard';
 
 configure({ adapter: new Adapter() });
 describe('App', () => {
-  xit('renders without crashing', () => {
+  it('renders without crashing', () => {
+    const wrapper = shallow( <App />);
+    expect(wrapper.find(Dashboard)).toHaveLength(1);
   });
 
   xit('matches the snapshot', () => {
